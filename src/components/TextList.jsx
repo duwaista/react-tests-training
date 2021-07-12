d02eae712710659664c2c748a0679740eda6c243
Vladislav Egorov <vlad.45.45@mail.ru>
2021-07-12 16:39:52 +0700

    Update tests

---
+++ src/components/TextList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function TextList({ list }) {
+  return (
+    <>
+      <div data-testid="text-component" className="text-container">
+        {list.map((item, index) => (
+          <span key={index} data-testid="list-item">
+            {item}
+          </span>
+        ))}
+      </div>
+      {!list.lenght && <span data-testid="empty-list">No text</span>}
+    </>
+  );
+}
--- src/components/TextList.test.js
+++ src/components/TextList.test.js
@@ -1,12 +1,23 @@
 import { render, screen } from "@testing-library/react";
 import TextList from "./TextList";
 
-test("Text List test", () => {
-  const text = "Test text";
+test("Text list test", () => {
+  const list = ["Booba", "Ooba", "Dooba", "Aboba"];
 
-  render(<TextList text={text} />);
-  const element = screen.getByTestId("text-component");
-  
-  expect(element).toBeInTheDocument();
-  expect(element).toHaveTextContent(text);
+  render(<TextList list={list} />);
+  const elements = screen.getAllByTestId("list-item");
+
+  expect(elements).toHaveLength(list.length);
+  for (let i; i <= elements.length; i++) {
+    expect(elements[i]).toBeInTheDocument();
+    expect(elements[i]).toHaveTextContent(list[i]);
+  }
+});
+test("Empty list test", () => {
+  const list = [];
+
+  render(<TextList list={list} />);
+  const element = screen.getByTestId("empty-list");
+
+  expect(element).toHaveTextContent("No text");
 });
